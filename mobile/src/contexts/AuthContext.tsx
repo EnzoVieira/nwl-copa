d9043cdbf6c0d5bfd912f7633a5e7eaaba80302e
Enzo Vieira <enzovieira16@hotmail.com>
2023-02-24 15:12:20 +0000

    feat(mobile): Authenticated routes

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,8 @@ import * as Google from "expo-auth-session/providers/google"
 import * as AuthSession from "expo-auth-session"
 import * as WebBrowser from "expo-web-browser"
 
+import { api } from "../services/api"
+
 WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
@@ -45,8 +47,25 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     }
   }
 
-  async function signInWithGoogle(accesToken: string) {
-    console.log("TOKEN DE AUTENTICACAO ", accesToken)
+  async function signInWithGoogle(accessToken: string) {
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post("/users", {
+        accessToken,
+      })
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${tokenResponse.data.token}`
+
+      const userInfoResponse = await api.get("/me")
+      setUser(userInfoResponse.data.user)
+    } catch (err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
